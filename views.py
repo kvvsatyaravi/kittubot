de31756293b3543ac40bba5d00030dd9f0d916af
kvvsatyaravi <32243517+kvvsatyaravi@users.noreply.github.com>
2019-12-25 15:56:50 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,16 @@
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from kittu.models import funday
+
+class Homeview(TemplateView):
+	Template_view="index.html"
+
+	def get(self,request):
+		return render(request,self.Template_view)
+
+	def post(self,request):
+		if request.method == 'POST':
+			num=request.POST.get('input',None)
+		context={'data':funday.objects.get(idnum=num)}
+		return render(request,self.Template_view,context)
